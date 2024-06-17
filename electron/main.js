d4f871bb8f25539c30f642ee24fe58c1b0b7e337
吴禹辉 <46028035+PointWu@users.noreply.github.com>
2024-06-17 10:54:59 +0800

    feat: init project

---
+++ electron/main.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require('electron')
+const createWindow = () => {
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600
+    })
+ 
+    win.loadURL('http://localhost:5173') // 修改访问地址
+    // win.loadFile('index.html')
+ 
+}
+app.whenReady().then(() => {
+    createWindow()
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
